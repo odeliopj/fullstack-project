af866d777f293fce22f240eaa97ae738a2b2a355
odeliopj <odeliopj@gmail.com>
2024-08-04 14:46:49 -0300

    Codígo até o cap 3.

---
+++ healthcheck.client/src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+@Component({
+  selector: 'app-fetch-data',
+  templateUrl: './fetch-data.component.html',
+  styleUrl: './fetch-data.component.css'
+})
+export class FetchDataComponent implements OnInit{
+  public forecasts: WeatherForecast[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getForecasts();
+  }
+
+  getForecasts() {
+    this.http.get<WeatherForecast[]>('/api/weatherforecast').subscribe(
+      (result) => {
+        this.forecasts = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+}
